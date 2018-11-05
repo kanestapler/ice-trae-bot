a55a8f6420d5f6533780116cdbc6eb48e7f8a38d
kanestapler <kanestapler@gmail.com>
2018-11-04 18:29:48 -0800

    Refactoring out all hard coded data

--- index.js
+++ index.js
@@ -1,31 +1,21 @@
 
 const rp = require('request-promise')
 require('dotenv').config()
-const GAME_ID = 'GAME_ID'
-const TEAM_ABBREVIATION = 'ATL'
-const SCHEDULE_URL = `http://site.web.api.espn.com/apis/site/v2/sports/basketball/nba/teams/${TEAM_ABBREVIATION}/schedule?region=us&lang=en&seasontype=`
-const GAMECAST_URL = `http://site.web.api.espn.com/apis/site/v2/sports/basketball/nba/summary?event=${GAME_ID}&lang=en&region=us&contentorigin=espn`
 const PRE_SEASON = '1'
 const REGULAR_SEASON = '2'
 const POST_SEASON = '3'
-const PLAYER_ID = '4277905'
-const SHOT_TYPE = {
-    'ThreePoint': '3PT',
-    'FieldGoal': 'FG',
-    'FreeThrow': 'FT'
-}
 
 function runPoller() {
     getCurrentGameID().then(function (gameID) {
         if (gameID) {
-            getMadeShots(gameID, SHOT_TYPE.ThreePoint).then(function(gameData) {
-                console.log(`Has made: ${gameData.numberOfMadeShots} ${SHOT_TYPE.ThreePoint} againt the ${gameData.opponent}`)
-                tellValidatorShotsMade(gameID, gameData.numberOfMadeShots, gameData.opponent).then(function(response){
+            getMadeShots(gameID, process.env.STAT_LABEL).then(function (gameData) {
+                console.log(`Has made: ${gameData.numberOfMadeShots} ${process.env.STAT_LABEL} againt the ${gameData.opponent}`)
+                tellValidatorShotsMade(gameID, gameData.numberOfMadeShots, gameData.opponent).then(function (response) {
                     console.log('Success to vali')
-                }).catch(function(error) {
+                }).catch(function (error) {
                     console.log('Error calling vali', error)
                 })
-            }).catch(function(error) {
+            }).catch(function (error) {
                 console.log('Error getting shots made from gameID', error)
             })
         } else {
@@ -47,30 +37,30 @@ function tellValidatorShotsMade(gameID, amount, opponent) {
             token: process.env.API_TOKEN
         },
         json: true // Automatically stringifies the body to JSON
-    };
+    }
     return rp.post(options)
 }
 
 function getMadeShots(gameID, shotType) {
-    url = getGameCastURL(gameID)
+    const url = getGamecastURL(gameID)
     return new Promise(function (resolve, reject) {
         rp.get(url).then(function (dataString) {
             const gamecastData = JSON.parse(dataString)
             const opponentName = getOpponentName(gamecastData)
             if (gamecastData.boxscore.players) {
-            const arrayPosition = getArrayPosition(shotType, gamecastData)
-            const allPlayersStats = gamecastData.boxscore.players.find(function (x) {
-                return x.team.abbreviation === TEAM_ABBREVIATION
-            })
-            const playerStats = allPlayersStats.statistics[0].athletes.find(function (x) {
-                return x.athlete.id === PLAYER_ID
-            })
-            const shotStats = playerStats.stats[arrayPosition]
-            resolve({numberOfMadeShots: getMadeShotsFromString(shotStats), opponent: opponentName})
-        } else {
-            // As far as I can tell this means game hasn't started yet
-            resolve({numberOfMadeShots: '0', opponent: opponentName})
-        }
+                const arrayPosition = getArrayPosition(shotType, gamecastData)
+                const allPlayersStats = gamecastData.boxscore.players.find(function (teamAndStats) {
+                    return isTeamFollowing(teamAndStats.team)
+                })
+                const playerStats = allPlayersStats.statistics[0].athletes.find(function (athleteProfile) {
+                    return athleteProfile.athlete.id === process.env.PLAYER_ID
+                })
+                const shotStats = playerStats.stats[arrayPosition]
+                resolve({ numberOfMadeShots: getMadeShotsFromString(shotStats), opponent: opponentName })
+            } else {
+                // As far as I can tell this means game hasn't started yet
+                resolve({ numberOfMadeShots: '0', opponent: opponentName })
+            }
         }).catch(function (error) {
             console.log('Error calling gamecast API', error)
             reject('Error calling gamecast API')
@@ -88,19 +78,15 @@ function getArrayPosition(shotType, gamecastData) {
     })
 }
 
-function getGameCastURL(gameID) {
-    return GAMECAST_URL.replace(GAME_ID, gameID)
-}
-
 function getCurrentGameID() {
     return new Promise(function (resolve, reject) {
-        rp.get(SCHEDULE_URL + REGULAR_SEASON).then(function (dataString) {
+        rp.get(getScheduleURL()).then(function (dataString) {
             const responseData = JSON.parse(dataString)
             const game = getCurrentGame(responseData.events)
             if (game) {
                 resolve(game.id)
             } else {
-                resolve()
+                resolve('401070809')
             }
         }).catch(function (error) {
             console.log('Error calling schedule API', error)
@@ -128,15 +114,33 @@ function checkIfDateIsWithin5HoursAndInThePast(dateString) {
 
 // a and b are javascript Date objects
 function dateDiffInHours(current, test) {
-    const MS_PER_HOUR = 1000 * 60 * 60;
-    return (current - test) / MS_PER_HOUR;
+    const MS_PER_HOUR = 1000 * 60 * 60
+    return (current - test) / MS_PER_HOUR
 }
 
 function getOpponentName(gamecastData) {
-    const opponent = gamecastData.boxscore.teams.find(function(team) {
-        return team.team.abbreviation != TEAM_ABBREVIATION
+    const opponent = gamecastData.boxscore.teams.find(function (teamAndStats) {
+        return !isTeamFollowing(teamAndStats.team)
     })
     return opponent.team.name
+}
+
+function isTeamFollowing(team) {
+    const followingTeam = process.env.TEAM
+    return (team.abbreviation === followingTeam || team.id === followingTeam)
+}
+
+function getScheduleURL() {
+    const sport = process.env.SPORT
+    const league = process.env.LEAGUE
+    const team = process.env.TEAM
+    return `http://site.web.api.espn.com/apis/site/v2/sports/${sport}/${league}/teams/${team}/schedule?region=us&lang=en&seasontype=${REGULAR_SEASON}`
+}
+
+function getGamecastURL(gameID) {
+    const sport = process.env.SPORT
+    const league = process.env.LEAGUE
+    return `http://site.web.api.espn.com/apis/site/v2/sports/${sport}/${league}/summary?event=${gameID}&lang=en&region=us&contentorigin=espn`
 }
 
 module.exports.handler = runPoller
